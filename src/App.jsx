4b2ea88256b648aee72908846d5954db8d51f676
Raony Melo <rcostamelo@gmail.com>
2025-10-31 10:14:54 -0300

    Add basename to Router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DiscordEditor from "./pages/DiscordEditor";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/whatsapp" element={<WhatsAppEditor />} />
